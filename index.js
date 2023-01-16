bf3820857e653196b03c6e871926c743de3822ca
Mark Pace <pace@illuminati.org>
2023-01-16 10:10:02 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 
+// un and pw
+const validun = 'user'
+const validpw = 'pass'
+
 var express = require('express');
 var bodyParser = require('body-parser');
 var fs = require('fs');
 var net = require('net');
 var app = express();
-const http = require('http').Server(app);
-
+//const http = require('http').Server(app);
 
 /*
 *   BEGINNING OF CONFIGURATION
@@ -15,7 +18,7 @@ const http = require('http').Server(app);
 *   HOST
 *
 */
-var host = "localhost";
+var host = "104.236.193.72";
 /*
 *   PORT
 *
@@ -31,26 +34,20 @@ var optionalURLResource = ""; // optional
 *
 */
 
-
-
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
 
-
-var useToken = false;
-
 app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
 
 // get POST to log In
-app.post('/logIn', function(request, response){
+app.post('/logIn', function(request, response) {
 
     console.log(request.body.username);
     console.log(request.body.password);
-	
-	response.send(JSON.stringify({"username":"streamUsername","password":"streamPassword","streamID":"streamID"}));
-	
+    response.send(JSON.stringify({"username":"streamUsername","password":"streamPassword","streamID":"streamID"}));
+
 });
 
 
@@ -58,49 +55,45 @@ app.post('/logIn', function(request, response){
 app.post('/validateCredentials', function(request, response){
 
     console.log('\n\nvalidate credentials called');
-	console.log('type: '+request.body.type);
+    console.log('type: '+request.body.type);
     console.log('username: '+request.body.username);
     console.log('password: '+request.body.password);
-	console.log('streamID: '+request.body.streamID);
-    if (useToken)
-        console.log('token: '+request.body.token);
+    console.log('streamID: '+request.body.streamID);
+    console.log('token: '+request.body.token);
 
-	var type = request.body.type;
-	
-	if (type == "publisher")
-		response.send(JSON.stringify({"result":true,"url":optionalURLResource}));
-	else if (type == "subscriber")
-		response.send(JSON.stringify({"result":true}));
-	else
-	{
-		console.log('invalid type supplied');
-		response.send(JSON.stringify({"result":false}));
-	}
+    var type = request.body.type;
+    var result = false;
+
+    if ((request.body.username == validun) && (request.body.password == validpw)) {
+        result = true;
+        console.log('valid un/pw');
+    } else {
+        console.log('invalid un/pw');
+    }
+
+    if (type == "publisher") {
+        response.send(JSON.stringify({"result":result,"url":optionalURLResource}));
+    } else if (type == "subscriber") {
+        response.send(JSON.stringify({"result":result}));
+    } else {
+        console.log('invalid type supplied');
+        response.send(JSON.stringify({"result":false}));
+    }
+
 });
 
 // get POST to invalidate the credentials for a stream
-app.post('/invalidateCredentials', function(request, response){
+app.post('/invalidateCredentials', function(request, response) {
 
     console.log('\n\ninvalidate credentials called');
     console.log('username: '+request.body.username);
     console.log('password: '+request.body.password);
-	console.log('streamID: '+request.body.streamID);
-    if (useToken)
-        console.log('token: '+request.body.token);
-	
-	response.send(JSON.stringify({"result":true}));
-	
+    console.log('streamID: '+request.body.streamID);
+    console.log('token: '+request.body.token);
+    response.send(JSON.stringify({"result":true}));
+
 });
-
-// capture general /webhook POSTs
-app.post('/webhook', function(request, response){
-
-    console.log('\n\nWebhook POST');
-    console.log(request.body);
-	
-	response.send({});
-});
-
 
 app.listen(port,host);
-console.log(`Server running on port ${port}`)
+
+console.log(`${host} running on ${port}.`)
